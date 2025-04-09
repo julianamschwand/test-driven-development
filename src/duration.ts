85307e5770641a26c210b74dcb1541de09c62578
julianamschwand <julian_amschwand@sluz.ch>
2025-04-09 14:08:57 +0200

    Aufgabe 1: Produktivecode 'formatDuration' hinzugefügt

--- src/duration.ts
+++ src/duration.ts
@@ -1,4 +1,11 @@
 export function formatDuration(seconds: number): string {
-  console.log(seconds);
-  throw new Error("not implemented yet");
+  let formattedDuration: string = ""
+
+  if (seconds / 3600 >= 1) formattedDuration += (Math.floor(seconds / 3600) + "h")
+  if (seconds / 60 >= 1 && seconds % 3600 !== 0) formattedDuration += (Math.floor((seconds % 3600) / 60) + "m")
+  if (seconds >= 0 && seconds % 60 !== 0) formattedDuration += (Math.round(seconds % 60) + "s")
+  if (seconds === 0) formattedDuration = "0s"
+  if (seconds < 0) throw new Error("Can't show the duration of negative numbers")
+  
+  return formattedDuration
 }
